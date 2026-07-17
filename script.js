6d72a8a01dc126824af90cc4cede75d974828a0e
sarow <sakthisaravanakania@gmail.com>
2026-07-17 06:55:06 +0530

    Added HTML

---
+++ script.js
@@ -0,0 +1,63 @@
+// 1 — Looping Through an Array
+
+        // for loop
+// const numbers = [10, 20, 30, 40];
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+       //forEach
+// const numbers = [10, 20, 30, 40];
+// numbers.forEach((num) => {
+//     console.log(num);
+// });
+
+    //  for of loop
+// const numbers = [10, 20, 30, 40];
+// for (let num of numbers) {
+//     console.log(num);
+// }
+
+    //  for in loop
+// const numbers = [10, 20, 30, 40];
+// for (let num in numbers) {
+//     console.log(numbers[num]);
+// }
+
+
+// 2 — Loop Through an Object
+
+// const student = {
+//     name: "Bala",
+//     age: 21,
+//     grade: "A"
+//   };
+// for (const key in student) {
+//     console.log(key, student[key]);
+// }
+
+    // 3 — Using map()
+
+// const marks = [50, 60, 70, 80];
+// const finalMarks = marks.map((mark) => {
+//     return mark - 10;
+// });
+
+// console.log(finalMarks);
+
+//    4 — Using filter()
+// const values = [5, 12, 8, 25, 3, 15];
+// const finalvalues = values.filter((values) => {
+//     return values > 10;
+// });
+// console.log(finalvalues );
+
+
+//    5 — Using reduce()
+
+// const nums = [5, 10, 15, 20];
+// const total = nums.reduce((sum, num) => {
+//     return sum + num;
+// }, 0);
+// console.log(total);   
+
